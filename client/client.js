a79501e3390cf403202d054d8119c9c12e1b9d39
Andre0x1 <andrel.silva3357@gmail.com>
2022-11-17 13:38:58 -0300

    Ajustes Index

--- client/client.js
+++ client/client.js
@@ -1,25 +1,82 @@
+const publicVapidKey =
+  "BKbvAgTVIKSN_NoSGAGls8COda1sXr-zYzUHFFENDTcQLHsH-pWA-U6AS8khh7jjGQs5rkmt-dQrgroU487eXO0";
 
-const publicVapidKey = "BOd2EQ8LTe3KAgMX9lWwTlHTRzv1Iantw50Mw6pUnsNr3pcxl8iglUs-YlQEQLo4UbJk9oyXs_BxgyAe0TCqKME";
+var user = 2;
 
-if('serviceWorker' in navigator) {
-    registerServiceWorker().catch(console.log)
-}
-
-async function registerServiceWorker() {
-    const register = await navigator.serviceWorker.register('./worker.js', {
-        scope: '/'
-    });
-
-    const subscription = await register.pushManager.subscribe({
+navigator.serviceWorker.register("./worker.js", { scope: "/" }).then(
+  async function (reg) {
+    var subs;
+    var serviceWorker;
+    if (reg.installing) {
+      serviceWorker = reg.installing;
+      // console.log('Service worker installing');
+    } else if (reg.waiting) {
+      serviceWorker = reg.waiting;
+      // console.log('Service worker installed & waiting');
+    } else if (reg.active) {
+      serviceWorker = reg.active;
+      subs = await reg.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: publicVapidKey,
-    });
+      });
+    }
 
-    await fetch("/subscribe", {
-        method: "POST",
-        body: JSON.stringify(subscription),
-        headers: {
-            "Content-Type": "application/json",
+    if (serviceWorker) {
+      console.log("sw current state", serviceWorker.state);
+      if (serviceWorker.state == "activated") {
+        console.log("sw already activated - Do watever needed here");
+
+        subs = await reg.pushManager
+          .subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: publicVapidKey,
+          })
+          .then(async function (resp) {
+            await fetch("/subscribe/" + user, {
+              method: "POST",
+              body: JSON.stringify(resp),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+          });
+      }
+      serviceWorker.addEventListener("statechange", async function (e) {
+        console.log("sw statechange : ", e.target.state);
+        if (e.target.state == "activated") {
+          // use pushManger for subscribing here.
+          console.log(
+            "Just now activated. now we can subscribe for push notification"
+          );
+          if (subs) {
+            await fetch("/subscribe/" + user, {
+              method: "POST",
+              body: JSON.stringify(subs),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+          } else {
+            subs = await reg.pushManager
+              .subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: publicVapidKey,
+              })
+              .then(async function (resp) {
+                await fetch("/subscribe/" + user, {
+                  method: "POST",
+                  body: JSON.stringify(resp),
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                });
+              });
+          }
         }
-    })
-}
+      });
+    }
+  },
+  function (err) {
+    console.error("unsuccessful registration with ", workerFileName, err);
+  }
+);
--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-const express = require('express');
-const webpush = require('web-push');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const webpush = require("web-push");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "client")))
+app.use(express.static(path.join(__dirname, "client")));
 
-const publicVapidKey = "BOd2EQ8LTe3KAgMX9lWwTlHTRzv1Iantw50Mw6pUnsNr3pcxl8iglUs-YlQEQLo4UbJk9oyXs_BxgyAe0TCqKME";
-const privateVapidKey = "4AoSsRHFaHv0Fupd2NRtrungJF2jkqgccTu-WEc781w";
+const data = webpush.generateVAPIDKeys();
 
-webpush.setVapidDetails("mailto:test@test.com", publicVapidKey, privateVapidKey);
+webpush.setVapidDetails(
+  "mailto:test@test.com",
+  data.publicKey,
+  data.privateKey
+);
 
-app.post('/subscribe', (req, res) => {
-    const subscription = req.body;
-    res.status(201).json({});
-    const payload = JSON.stringify({ title: "Hello World", body: "This is your first push notification" });
-    webpush.sendNotification(subscription, payload).catch(console.log);
-})
+console.log(data);
+
+app.post("/subscribe/1", (req, res) => {
+  console.log(req.body);
+  const subscription = req.body;
+  res.status(201).json({});
+  const payload = JSON.stringify({
+    title: "Teste1",
+    body: "Teste1",
+  });
+  webpush.sendNotification(subscription, payload);
+});
+
+app.post("/subscribe/2", (req, res) => {
+  console.log(req.body);
+  const subscription = req.body;
+  res.status(201).json({});
+  const payload = JSON.stringify({
+    title: "Teste2",
+    body: "Teste2",
+  });
+  webpush.sendNotification(subscription, payload);
+});
 
 const PORT = 5000;
 
 app.listen(PORT, () => {
-    console.log("Server started on port " + PORT);
-})
+  console.log("Server started on port " + PORT);
+});
